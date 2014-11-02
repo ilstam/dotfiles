5102e0d22508706e661cb628b41c974ee46377f6
Ilias Stamatis <stamatis.iliass@gmail.com>
2014-11-02 20:33:47 +0200

    keep old dotfiles

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 export DOTFILES=$HOME/.dotfiles
-if [ $(dirname $0) != $DOTFILES ]; then
-	cp -R $(dirname $0) $DOTFILES
+if [ $(dirname $0) != "$DOTFILES" ]; then
+	if [ -d "$DOTFILES" ]; then
+		mv "$DOTFILES" "${DOTFILES}old"
+	fi
+	cp -R $(dirname $0) "$DOTFILES"
 fi
 
 $DOTFILES/git/install.sh
