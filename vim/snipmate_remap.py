c094e52784295303337a883d833d77a52143c2b0
Ilias Stamatis <stamatis.iliass@gmail.com>
2014-11-03 02:31:15 +0200

    make script python2 compatible

--- vim/snipmate_remap.py
+++ vim/snipmate_remap.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Simple script for remapping snipMate plugin's default trigger key from tab
 # to other, in order to not mess up with neocomplcache plugin's functionality.
 # Note: It doesn't work for all keys.
+from __future__ import with_statement
 
 import os
 import shutil
@@ -10,7 +11,7 @@ import fnmatch
 # new trigger key
 TRIGGER_KEY = '*'
 # your snipmate directory location
-SNIPMATE_DIR = os.getenv('HOME') + '/.vim/bundle/snipmate.vim/'
+SNIPMATE_DIR = '/home/ilias/.vim/bundle/snipmate.vim/'
 
 files = []
 for root, dirnames, filenames in os.walk(SNIPMATE_DIR):
@@ -19,9 +20,10 @@ for root, dirnames, filenames in os.walk(SNIPMATE_DIR):
 
 for a_file in files:
     tmp_file = a_file + '~'
-    with open(a_file, mode='r') as f1, open(tmp_file, mode="w") as f2:
-        content = f1.read()
-        content = content.replace('<tab>', TRIGGER_KEY)
-        content = content.replace('<s-tab>', '<s-{0}>'.format(TRIGGER_KEY))
-        f2.write(content)
+    with open(a_file, mode='r') as f1:
+        with open(tmp_file, mode="w") as f2:
+            content = f1.read()
+            content = content.replace('<tab>', TRIGGER_KEY)
+            content = content.replace('<s-tab>', '<s-{0}>'.format(TRIGGER_KEY))
+            f2.write(content)
     shutil.move(tmp_file, a_file)
