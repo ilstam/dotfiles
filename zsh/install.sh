8263981add854c34228a14357fbc5500bddd7517
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-06-02 17:03:15 +0300

    ask user whether to make zsh the default shell or not

--- zsh/install.sh
+++ zsh/install.sh
@@ -36,4 +36,10 @@ if [ ! -f ~/.oh-my-zsh/themes/mytheme.zsh-theme -a ! -h ~/.oh-my-zsh/themes/myth
 	ln -s "$DOTFILES"/zsh/mytheme.zsh-theme ~/.oh-my-zsh/themes/mytheme.zsh-theme
 fi
 
+echo -n "--> do you want to make zsh your default shell? (y/n) "
+an=$(read a && echo "$a" | tr '[:upper:]' '[:lower:]')
+if [ -z "$an" -o "$an" == "y" -o "$an" == "yes" ]; then
+	chsh -s /bin/zsh
+fi
+
 echo -e "\ndone...\n"
