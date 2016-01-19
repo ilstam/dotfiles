c701d2dd63c279a7df2e6ee4615414bc3fac288d
Ilias Stamatis <stamatis.iliass@gmail.com>
2016-01-19 14:13:17 +0200

    color the output when new mails

--- scripts/unread_mails.sh
+++ scripts/unread_mails.sh
@@ -7,7 +7,13 @@ COUNT=`curl -su $USER:$PASS https://mail.google.com/mail/feed/atom || echo "<ful
 COUNT=`echo "$COUNT" | grep -oPm1 "(?<=<fullcount>)[^<]+" `
 
 if [ -z "$COUNT" ]; then
-	echo " no access"
+	echo " no access" # full text
+	echo " no access" # short text
 else
-	echo " $COUNT new"
+	echo " $COUNT new" # full text
+	echo " $COUNT new" # short text
+
+	if [ "$COUNT" -gt 0 ]; then
+		echo \#FFFC00 # colored output
+	fi
 fi
