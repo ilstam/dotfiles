a2295c30b63ff8f636eac38aacf5e322c63d9915
Ilias Stamatis <ilstam@mailbox.org>
2024-05-06 21:20:09 +0100

    git - add gitconfig.work

--- git/install.sh
+++ git/install.sh
@@ -7,11 +7,22 @@ fi
 echo -e "configuring git...\n"
 
 if [ -f ~/.gitconfig ]; then
-	echo "--> backup existing ~/.gitconfig file to ~/gitconfig.orig"
+	echo "--> backup existing ~/.gitconfig file to ~/.gitconfig.orig"
 	mv ~/.gitconfig ~/.gitconfig.orig
 fi
 
 echo "--> symlink ~/.gitconfig to the real gitconfig"
 ln -s "$DOTFILES/git/gitconfig" ~/.gitconfig
+
+# Is this a work machine?
+if [ -e /apollo ]; then
+	if [ -f ~/.gitconfig.local ]; then
+		echo "--> backup existing ~/.gitconfig.local file to ~/.gitconfig.local.orig"
+		mv ~/.gitconfig.local ~/.gitconfig.local.orig
+	fi
+
+	echo "--> copying gitconfig.work to ~/.gitconfig.local"
+	cp "$DOTFILES/git/gitconfig.work" ~/.gitconfig.local
+fi
 
 echo -e "\ndone...\n"
